35fbdc68e173b9b3dadb912a44769cd97c09213d
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2018-10-29 12:30:50 +0100

    lightsensortest

---
+++ robot/src/lightsensortest.py
@@ -0,0 +1,29 @@
+from ev3dev.ev3 import ColorSensor
+import time
+
+sL = ColorSensor('in3')
+sR = ColorSensor('in2')
+
+charcount = 40
+mi = 0
+ma = 100
+
+
+def progressbar(val):
+    t = (val - mi) / (ma - mi)
+    markedcount = round(charcount * t)
+    return '#' * markedcount + ' ' * (charcount - markedcount)
+
+
+def intstr(val):
+    return ('   ' + str(val))[-3:]
+
+
+while True:
+    l, r = sL.value(), sR.value()
+    print(
+        "L:" + intstr(l) + "|" + progressbar(l) + "|, " +
+        "R:" + intstr(r) + "|" + progressbar(r) + "|",
+        end='\r'
+    )
+    time.sleep(0.1)
