f749164333188438980fc9543e9d5b5dee087546
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2018-11-16 17:56:53 +0100

    StateNode test

--- solver/StateNode.py
+++ solver/StateNode.py
@@ -1,4 +1,3 @@
-
 class StateNode:
     def __init__(self, agent, diamonds, cost):
         self.agent = agent
@@ -6,4 +5,10 @@ class StateNode:
         self.current_cost = cost
 
     def __hash__(self):
-        return hash(self.agent + self.diamonds)
+        return hash(self.agent + (*self.diamonds,))
+
+
+if __name__ == '__main__':
+    node = StateNode((3, 3, 0), ((1, 1), (1, 0), (0, 1), (2, 1)), 0)
+    assert node.__hash__() == hash((3, 3, 0, (0, 1), (1, 0), (1, 1), (2, 1))), node.__hash__()
+    print('Test succeeded')
