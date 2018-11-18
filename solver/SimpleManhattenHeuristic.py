6dead9cf0f88eb907c86fe0d7d11f8e519975dba
jannes <jannes.helck@gmail.com>
2018-11-18 16:14:26 +0100

    modificated: SimpleManhattenHeuristics, new class: checkGoal

--- solver/SimpleManhattenHeuristic.py
+++ solver/SimpleManhattenHeuristic.py
@@ -2,29 +2,30 @@ import numpy as np
 
 
 class SimpleManhattenHeuristic:
-    def __init__(self, map, init_state):
-        pass
+    def __init__(self, _map, init_state):
+        self.map = _map
 
     def __call__(self, state):
+
+        self.diamonds = state     
         self.manhatten = 0
         self.buffer = []
+        self.goals =  np.transpose(self.map.count(b'G').nonzero())
 
-        self.diamond_mask = self.map.count(b'J')
-        self.diamonds = np.transpose(np.nonzero(self.diamond_mask))
+        print(self.diamonds)
+        print(self.goals)
 
-        self.goal_mask = self.map.count(b'G')
-        self.goal_mask += self.map.count(b'W')
-        self.goals = np.transpose(np.nonzero(self.goal_mask))
-
-        if (np.size(self.diamonds) >= 4):
-            for i in range(np.shape(self.diamonds)[0]):
-                for j in range(np.shape(self.diamonds)[1]):
-                    self.buffer.append(
-                        abs(self.diamonds[i, 0] - self.goals[j, 0]) + abs(self.diamonds[i, 1] - self.goals[j, 1]))
+        if (np.size(self.goals) >= 4):
+            for i in range(np.shape(self.goals)[0]):
+                for j in range(np.shape(self.goals)[1]):
+                    self.buffer.append(abs(self.diamonds[i][0] - self.goals[j, 0]) + abs(self.diamonds[i][1] - self.goals[j, 1]))
 
                 self.manhatten += min(self.buffer)
                 del self.buffer[:]
 
-        elif (np.size(self.diamonds) == 2):
+        elif (np.size(self.goals) == 2):
             self.manhatten = abs(self.diamonds[0][0] - self.goals[0][0]) + abs(
                 self.diamonds[0][1] - self.goals[0][1])
+
+
+        return self.manhatten
