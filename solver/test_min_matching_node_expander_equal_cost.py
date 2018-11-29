179d8d0080af7cd0a1477c2809324a357d3ef542
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2018-11-29 13:16:21 +0100

    added another min_matching test and fixed a related bug

--- solver/test_min_matching_node_expander_equal_cost.py
+++ solver/test_min_matching_node_expander_equal_cost.py
@@ -1,27 +1,47 @@
 """
 The min-matching heuristic should calculate the actual
-cost of test-map3 from the initial state.
+cost of test-map3 and test-map4 from the initial states.
 """
 
 from MapLoader import load_map
 from Solver import solve
 from AgentStateNodeExpander import AgentStateNodeExpander as NodeExpander
-from ClosestHeuristic import ClosestHeuristic as Heuristic
+from MinMatchingHeuristic import MinMatchingHeuristic as Heuristic
 from UnitCost import default_unit_cost
 
-_map, initial_states = load_map("test-map3.txt")
+c = {
+    'f': default_unit_cost.forward,
+    't': default_unit_cost.turn,
+    'u': default_unit_cost.u_turn,
+    'F': default_unit_cost.forward_diamond,
+    'p': default_unit_cost.push
+}
 
-end_node = solve(_map, initial_states, NodeExpander, Heuristic, default_unit_cost)
+for map_path, opt_sol in (
+        ('test-map3.txt', 'ffFFFFFprfFFprfFFFFFF'),
+        ('test-map4.txt', 'fFplfprfprf')
+):
+    print('Map:', map_path)
+    _map, initial_states = load_map(map_path)
 
-heuristic = Heuristic(_map, default_unit_cost)
+    end_node = solve(_map, initial_states, NodeExpander, Heuristic, default_unit_cost)
 
-min_h = float('inf')
-for node in initial_states:
-    h = heuristic(node)
-    if h < min_h: min_h = h
+    heuristic = Heuristic(_map, default_unit_cost)
+    min_h = float('inf')
+    for node in initial_states:
+        h = heuristic(node)
+        if h < min_h: min_h = h
 
-print('Init heuristic:', min_h)
-print('Solution cost:', end_node.current_cost)
-diff = min_h - end_node.current_cost
-assert diff < 1e-10, 'test failed: {}'.format(diff)
-print('test succeeded')
+    exp_cost = 0
+    opt_sol = opt_sol.replace('ll', 'u').replace('rr', 'u').replace('l', 't').replace('r', 't')
+    for s in opt_sol:
+        exp_cost += c[s]
+
+    print('Expected cost:', exp_cost)
+    print('Init heuristic:', min_h)
+    print('Solution cost:', end_node.current_cost)
+    h_diff = exp_cost - min_h
+    s_diff = exp_cost - end_node.current_cost
+    assert abs(h_diff) < 1e-10, 'heuristic test failed: {}'.format(h_diff)
+    assert abs(s_diff) < 1e-10, 'solution test failed: {}'.format(s_diff)
+    print('test succeeded\n')
